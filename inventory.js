e92e981b402e34071dcb6b065873d9cd922dcf11
kcsheng <kacysheng@mail.com>
2021-05-28 19:37:27 +1000

    Usage of exports

---
+++ inventory.js
@@ -0,0 +1,11 @@
+const stationery = ["stickytape", "notebook", "pen"];
+const furnitures = ["desk", "chair"];
+
+// console.log(items);
+
+module.exports = {
+  stationery,
+  furnitures,
+};
+
+// console.log(module);
---
+++ main.js
@@ -0,0 +1,9 @@
+// const inventory = require("./inventory");
+
+// console.log(inventory);
+// console.log(inventory.stationery);
+// console.log(inventory.furnitures);
+
+const { stationery, furnitures } = require("./inventory");
+
+console.log(stationery, furnitures);
