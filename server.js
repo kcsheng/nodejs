9900d2aad96cbb7fce32a3910a566f4776ff560d
kcsheng <kacysheng@mail.com>
2021-05-31 22:32:17 +1000

    Incorporate html routes

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // set header content type
   res.setHeader("Content-Type", "text/html");
 
-  res.write("<head><title>Server Test</title></head>");
-  res.write("<head><link href='#' type='stylesheet'></head>");
-  res.write("<p>Hi there!</p>");
-  res.write("<p>Hi there again!</p>");
-  res.end();
+  //set up routes
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = "200";
+      break;
+    case "/index":
+      res.statusCode = "301";
+      res.setHeader("Location", "/");
+      res.end();
+      break;
+    case "/index.html":
+      res.statusCode = "301";
+      res.setHeader("Location", "/");
+      res.end();
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = "200";
+      break;
+    case "/about-me":
+      res.statusCode = "301";
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = "404";
+      break;
+  }
+  //send html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    // res.write(data);
+    res.end(data);
+  });
 });
 
 server.listen(3000, "localhost", () => {
