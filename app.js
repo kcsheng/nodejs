5d1b41ce0751430b675e5f0b4cef481732665c51
kcsheng <kacysheng@mail.com>
2021-06-02 15:18:30 +1000

    Incorporate ejs as view(template) engine

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const express = require("express"); // returns a function
 // create an instance of express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs"); // will automatically look for .ejs files in views
+
 // listen for requests
 app.listen(3000); // This also creates an instance of server (amazing!)
 
 // Now we can respond to url
 app.get("/", (req, res) => {
-  // res.send("<p>Home page</p>"); // Automatically sense and set the header/status code.
-  res.sendFile("./views/index.html", { root: __dirname }); // has to specify absolute path
+  res.render("index"); // This will render a view. Specify where the ejs file is.
 });
 
 app.get("/about", (req, res) => {
-  // res.send("<p>About page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-//redirect
-app.get("about-me", (req, res) => {
-  res.redirect("/about"); // This saves us so much work with setHeader and status code.
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 page (needs to be at the bottom of all routes)
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
-}); // This basically says, if there is no match so far, USE this callback function.
+  res.status(404).render("404");
+});
