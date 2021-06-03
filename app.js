a5cf0bef67c20f295b0e999b53b1d881ab1868f8
kcsheng <kacysheng@mail.com>
2021-06-04 09:11:29 +1000

    Finalise create blog functionality and display

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require("express"); 
+const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog")
-
+const Blog = require("./models/blog");
 
 const app = express();
 
 // connect to mongo db
-const dbURI = "mongodb+srv://kcsheng:80Bostus0612@clusternodejs.o5o1a.mongodb.net/nodejs?retryWrites=true&w=majority"
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => { app.listen(3000); })
-  .catch((err) => { console.log(err); }) // the connection returns a promise.
+const dbURI =
+  "mongodb+srv://kcsheng:80Bostus0612@clusternodejs.o5o1a.mongodb.net/nodejs?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  }); // the connection returns a promise.
 
-app.set("view engine", "ejs"); 
-
- 
+app.set("view engine", "ejs");
 
 // middleware and static files
-app.use(express.static("public")); // express middleware 
+app.use(express.static("public")); // express middleware
+app.use(express.urlencoded({ extended: true })); // encode the form data
 app.use(morgan("dev"));
 
 // generic routes
 app.get("/", (req, res) => {
-    res.redirect("/blogs")
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
@@ -32,9 +36,22 @@ app.get("/about", (req, res) => {
 // Blog routes
 
 app.get("/blogs", (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
+  Blog.find()
+    .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result })
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
     })
     .catch((err) => {
       console.log(err);
