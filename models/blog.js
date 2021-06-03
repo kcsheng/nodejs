31f1d1cf77d4bfabef761a361badf3abc5f69202
kcsheng <kacysheng@mail.com>
2021-06-03 10:59:11 +1000

    Create a blog model

---
+++ models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  snippet: {
+    type: String,
+    requried: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const Blog = mongoose.model("Blog", blogSchema); // Look for 'blogs' in the database nad hooks up with schema
+module.exports = Blog;
