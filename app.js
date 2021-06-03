92591f49ca3acf92c71b44829b04e33e4d83b8e6
kcsheng <kacysheng@mail.com>
2021-06-03 10:26:15 +1000

    Set up mongoose connection

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express"); 
 const morgan = require("morgan");
-
+const mongoose = require("mongoose");
 
 
 const app = express();
 
 // connect to mongo db
 const dbURI = "mongodb+srv://kcsheng:80Bostus0612@clusternodejs.o5o1a.mongodb.net/notejs?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => { app.listen(3000); })
+  .catch((err) => { console.log(err); })
 
 app.set("view engine", "ejs"); 
 
-app.listen(3000); 
+ 
 
 // middleware and static files
 app.use(express.static("public")); // express middleware 
